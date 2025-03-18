4d53d594bd3705f27dfa97a9d781e59a8645db7c
shrikant sharma <wesome.org@gmail.com>
2025-03-18 23:26:25 +0530

    hello world

--- Hello World/src/main/java/org/wesome/dsalgo/HelloWorld.java
+++ Hello World/src/main/java/org/wesome/dsalgo/HelloWorld.java
@@ -3,5 +3,10 @@ package org.wesome.dsalgo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld.main");
+        hello("hello world");
+    }
+
+    public static void hello(String hello) {
+        System.out.println(hello);
     }
 }
---
+++ Hello World/src/test/java/org/wesome/dsalgo/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package org.wesome.dsalgo;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+    @Test
+    void testHello() {
+        HelloWorld.hello("hello world");
+    }
+}
